40c71b0dbe9304ee814c1002ac560dd262ba9667
roboticlass <38188406+roboticlass@users.noreply.github.com>
2018-04-11 11:46:58 +0200

    New Version

--- parser.py
+++ parser.py
@@ -4,16 +4,8 @@ import sqlite3
 
 
 def fun0(filename):
-    with open(filename, 'r') as f_read:
-        return f_read.read().split('\n')
-
-
-def fun1(file_contents):
-    return [ x for x in file_contents if len(x) > 0]
-
-
-def fun2(file_contents):
-    return [ x.split('\t') for x in file_contents if x[0] != '#' ]
+    with gzip.open(filename, 'rt') as f_read:
+        return [line.split('\t') for line in f_read if len(line)>0 and not line.startswith('#')]
 
 
 def fun3(file_contents):
@@ -28,28 +20,21 @@ def fun4(cursor, parsed):
 
 def fun5(cursor, parsed, table_columns):
     """ First Version: Based on executemany """
-    sql_aux = "INSERT INTO " + table_columns
+    sql_aux = "INSERT INTO " + table_columns+ " VALUES(" + "?," * 119 + "?)"
     parsed_t = tuple(map(tuple, parsed[1:]))
-    cursor.executemany(sql_aux + " VALUES(" + "?," * 119 + "?)", parsed_t)
+    cursor.executemany(sql_aux , parsed_t)
 
 
-def main_func():
-
-    """ AMPLIACIÓN 1"""
-    # Nombre del fichero a parsear
-    # Bucle que busque los distintos ficheros a parsear, crear una lista que contenga las direcciones
-    # Tipo PATH/TCGA.BLCA.mutect.0e239d8f-47b0-4e47-9716-e9ecc87605b9.DR-10.0.somatic.maf
-    filename = "TCGA.BLCA.mutect.0e239d8f-47b0-4e47-9716-e9ecc87605b9.DR-10.0.somatic.maf"
+def main_func(filename):
 
     # Comenzando parseo
-    print("Comenzando parseo fichero: " + filename)
+    print("Parsing file: " + filename)
 
     # Objetos de conexión con la base de datos
     # connection permite hacer .commit() .close()
     # cursor permite .execute("sql") .executemany("sql") .fetchone() .fetchall()
     connection = sqlite3.connect("biodata.db")
     cursor = connection.cursor()
-    cursor.execute('DROP TABLE IF EXISTS MAF')
 
     # Función 0: Lectura del fichero filename
     file_contents = fun0(filename)
@@ -76,9 +61,35 @@ def main_func():
     connection.close()
 
     # Parseo terminado
-    print("Terminado parseo fichero: " + filename)
+    print("Finishing file: " + filename + '\n\n')
 
-""" AMPLIACION 2: AÑADIR SOPORTE PARA MYSQL O BASE DE DATOS REMOTA """
-main_func(nombre)
+
+if __name__=='__main__':
+    import os
+
+    """ WE START DELETING MAF TABLE IF EXISTS"""
+    conn = sqlite3.connect("biodata.db")
+    cursor = conn.cursor()
+    cursor.execute('DROP TABLE IF EXISTS MAF')
+    conn.commit(); conn.close()
+
+    print('Starting parsing process: GETTING PATHs ')
+
+    """ First Version: We find out all the .maf.gz files within the working directory"""
+    path = []
+    for dirname, dirnames, filenames in os.walk('.'):
+        # print path to all subdirectories first.
+        for subdirname in dirnames:
+            for subsubdirname, subsubdirnames, sfilenames in os.walk(subdirname):
+                for filename in sfilenames:
+                    if '.gz' in filename:
+                        path.append(".\\" + subdirname + "\\" + filename)
+
+    print('PATHs READY\n\nStarting processing of files: ')
+
+    for x in path:
+        main_func(x)
+
+    print('ALL FILES PARSED\n')
 
 
